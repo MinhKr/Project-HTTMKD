0edc42fb160580494b73ffa359a329457ebada7f
KaTm <93910089+phamquockhanh1@users.noreply.github.com>
2025-01-09 16:50:40 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
+
+use App\Http\Controllers\ChatBotController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WeatherController;
+use App\Http\Controllers\GeminiController;
 
+Route::get('/gem', function () {
+    return view('gemini');
+});
 // Route hiển thị thời tiết
 Route::get('/', [WeatherController::class, 'index'])->name('weather.index');
 
 // Route gửi câu hỏi đến ChatGPT
-Route::post('/chat', [WeatherController::class, 'sendToChatGPT'])->name('chat.send');
+Route::post('/chat', [ChatBotController::class, 'sendToChatGPT']);
+Route::post('/question', [GeminiController::class, 'sendToGemini']);
